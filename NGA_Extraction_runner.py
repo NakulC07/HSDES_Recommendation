4a9447597f06131f1910c18b2e143efaae54c141
Choudhari, Nakul <nakul.choudhari@intel.com>
2025-02-03 15:16:51 +0530

    updated

--- NGA_Extraction_runner.py
+++ NGA_Extraction_runner.py
@@ -6,14 +6,16 @@ app_reg_id = '2e75abe8-764a-4773-9433-d064c27eacbf'
 app_reg_secret = 'Uix0KsdXU0Zzv3pN2hmPLHJ+Ti]V?v_i'
 
 # Prompt the user for the project name
-project_name = input("Enter project name in format e.g. (nga_fv_gnr): ").strip()
+projects = ['nga_fv_gnr' , 'nga_fv_gnrd']
 
 # Store the project name in a temporary file
-with open("current_project.txt", "w") as file:
-    file.write(project_name)
 
-# Define the output directory based on the project name
-output_dir = f"./{project_name}"
+for project_name in projects:
+    with open("current_project.txt", "w") as file:
+        file.write(project_name)
 
-# Extract data for the project
-extract_data_for_project(project_name, app_reg_id, app_reg_secret, output_dir)
+    # Define the output directory based on the project name
+    output_dir = f"./{project_name}"
+
+    # Extract data for the project
+    extract_data_for_project(project_name, app_reg_id, app_reg_secret, output_dir)
