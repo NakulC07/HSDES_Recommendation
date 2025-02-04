f67afee9e8e840dee3b5176c487560ba2ba3c24c
Choudhari, Nakul <nakul.choudhari@intel.com>
2025-02-04 10:54:57 +0530

    Proxy updates

--- NGA_DB_runner.py
+++ NGA_DB_runner.py
@@ -1,18 +1,18 @@
 import os
 from NGA_Extraction_DB import extract_data_for_project
 
-#proxy
+# Proxy settings
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["http_proxy"] = "http://proxy-chain.intel.com:912"
 os.environ["https_proxy"] = "http://proxy-chain.intel.com:912"
-
+os.environ["NO_PROXY"] = "login.microsoftonline.com"
 
 # Configuration
 app_reg_id = '2e75abe8-764a-4773-9433-d064c27eacbf'
 app_reg_secret = 'Uix0KsdXU0Zzv3pN2hmPLHJ+Ti]V?v_i'
 
 # List of projects
-projects = ['nga_fv_gnr' , 'nga_fv_gnrd']
+projects = ['nga_fv_gnr', 'nga_fv_gnrd']
 
 for project in projects:
     df = extract_data_for_project(project, app_reg_id, app_reg_secret)
